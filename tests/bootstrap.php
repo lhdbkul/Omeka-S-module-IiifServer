ac01ad4d5870b216899b79fc827ad92c79b3b6f2
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-07-13 00:00:00 +0000

    Updated tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,7 +3,9 @@
 require dirname(__DIR__, 3) . '/modules/Common/tests/Bootstrap.php';
 
 \CommonTest\Bootstrap::bootstrap(
-    ['Common', 'IiifServer'],
+    // ?DigitalObject: registered globally via STI on resource, its table must
+    // exist for resource value queries (linked resources).
+    ['Common', 'IiifServer', '?DigitalObject'],
     'IiifServerTest',
     __DIR__ . '/IiifServerTest'
 );
